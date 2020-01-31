96851feb787ad5aa2b940ef8b726e0b481c397dd
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2020-01-31 22:00:02 +0100

    db fix

--- data/install.sql
+++ data/install.sql
@@ -95,7 +95,7 @@ INSERT INTO `core_form_tab` (`Tab_ID`, `form`, `title`, `subtitle`, `icon`, `cou
 -- TAG - Core Form Field
 --
 
-INSERT INTO `core_form_field` (`Field_ID`, `type`, `label`, `fieldkey`, `tab`, `form`, `class`, `url_view`, `url_ist`, `show_widget_left`, `allow_clear`, `readonly`, `tbl_cached_name`, `tbl_class`, `tbl_permission`) VALUES
+INSERT INTO `core_form_field` (`Field_ID`, `type`, `label`, `fieldkey`, `tab`, `form`, `class`, `url_view`, `url_list`, `show_widget_left`, `allow_clear`, `readonly`, `tbl_cached_name`, `tbl_class`, `tbl_permission`) VALUES
 (NULL, 'text', 'Name', 'tag_label', 'tag-base', 'tag-single', 'col-md-3', '/tag/view/##ID##', '', 0, 1, 0, '', '', ''),
 (NULL, 'text', 'Key', 'tag_key', 'tag-base', 'tag-single', 'col-md-3', '/tag/view/##ID##', '', 0, 1, 1, '', '', ''),
 (NULL, 'partial', 'Entity Tags', 'entitytags', 'tag-entitytags', 'tag-single', 'col-md-12', '', '', '0', '1', '0', '', '', ''),
