7151ebe98c51114df7eeb4c7182927784e8dc890
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2021-03-16 21:47:01 +0100

    remove echos

--- src/Model/EntityTagTable.php
+++ src/Model/EntityTagTable.php
@@ -188,28 +188,38 @@ class EntityTagTable extends CoreEntityTable
             $sTag = substr($sTag,0,strlen($sTag)-strlen('_idfs'));
         }
 
-        echo 'save tag '.$sTag;
         # get tag
         $oTag = CoreController::$aCoreTables['core-tag']->select(['tag_key'=>$sTag]);
         if(count($oTag) > 0) {
-            echo 'got tag';
             $oTag = $oTag->current();
-            $aData = [
+
+            # Prevent duplicates
+            $oEntCheck = CoreController::$aCoreTables['core-entity-tag']->select([
                 'entity_form_idfs'=>$sForm,
-                'tag_idfs'=>$oTag->Tag_ID, // get id from core_tag
                 'tag_value'=>$sLabel,
-                'parent_tag_idfs'=>0,
-                'created_by'=>CoreController::$oSession->oUser->getID(),
-                'created_date'=>date('Y-m-d H:i:s',time()),
-                'modified_by'=>CoreController::$oSession->oUser->getID(),
-                'modified_date'=>date('Y-m-d H:i:s',time()),
-            ];
+            ]);
+            if(count($oEntCheck) == 0) {
+                $aData = [
+                    'entity_form_idfs'=>$sForm,
+                    'tag_idfs'=>$oTag->Tag_ID, // get id from core_tag
+                    'tag_value'=>$sLabel,
+                    'parent_tag_idfs'=>0,
+                    'created_by'=>CoreController::$oSession->oUser->getID(),
+                    'created_date'=>date('Y-m-d H:i:s',time()),
+                    'modified_by'=>CoreController::$oSession->oUser->getID(),
+                    'modified_date'=>date('Y-m-d H:i:s',time()),
+                ];
 
-            # Insert Tag
-            CoreController::$aCoreTables['core-entity-tag']->insert($aData);
+                # Insert Tag
+                CoreController::$aCoreTables['core-entity-tag']->insert($aData);
 
-            # Return ID
-            return CoreController::$aCoreTables['core-entity-tag']->lastInsertValue;
+                # Return ID
+                return CoreController::$aCoreTables['core-entity-tag']->lastInsertValue;
+            } else {
+                $oEntCheck = $oEntCheck->current();
+                return $oEntCheck->Entitytag_ID;
+            }
+
         } else {
             return 0;
         }
